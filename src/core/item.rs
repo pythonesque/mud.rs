4a044e47196835ac559a1ae69d18e9930b566bea
Joshua Yanovski <pythonesque@gmail.com>
2014-07-19 17:07:55 -0700

    Copy on write items... not quite working correcty yet.

---
+++ src/core/item.rs
@@ -0,0 +1,31 @@
+use std::sync::Arc;
+use std::fmt;
+
+#[deriving(Clone,Show)]
+pub struct ItemData /*{
+}*/;
+
+#[deriving(Clone)]
+pub struct Item {
+    data: ItemData,
+    contents: Arc<Vec<Item>>,
+}
+
+impl fmt::Show for Item {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Item data={} contents={}", self.data, self.contents.deref())
+    }
+}
+
+impl Item {
+    pub fn make(data: ItemData) -> Item {
+        let contents = Arc::new(Vec::new());
+        Item { data: data, contents: contents }
+    }
+
+    pub fn add<'a>(&'a self, item: Item) -> Item {
+        let mut contents = self.contents.clone();
+        contents.make_unique().push(item);
+        Item { data: self.data, contents: contents }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,26 @@
+#![feature(phase)]
+#[phase(plugin)] extern crate green;
+
+use core::item::{Item, ItemData};
+use std::sync::Arc;
+
+mod core;
+
+green_start!(main)
+
 fn main() {
+    let item1 = Arc::new(Item::make(ItemData));
+    let item1_clone = item1.clone();
+    spawn(proc() {
+        let item1 = item1_clone;
+        for _ in range(0,10000u) {
+            let item1_clone = item1.clone();
+            spawn(proc() {
+                let item1 = item1_clone;
+                let item2 = Item::make(ItemData);
+                item1.add(item2);
+            });
+        }
+    });
+    println!("{}", item1.deref());
 }
