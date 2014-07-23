fc52f6330c2a1f72777b9c16dbefb5070f6fab9d
Joshua Yanovski <pythonesque@gmail.com>
2014-07-22 17:35:16 -0700

    Fairly minimal testcase.

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,14 @@
-#![feature(macro_rules)]
-#![feature(phase)]
 #![feature(unsafe_destructor)]
-use std::comm::Messages;
-use std::fmt;
-use std::io::{ChanReader, ChanWriter};
-
-pub type CapType = ();
-
-pub trait Command<T: fmt::Show>: fmt::Show + Send {
-}
-
-enum CmdWrap<C> {
-    Write(Box<Writer + Send>),
-}
-
-pub trait CapRef<C>: fmt::Show {
-}
-
-struct CapTaskRef<C> {
-    tx: Sender<CmdWrap<C>>,
-}
-
-impl<C: Send> CapRef<C> for CapTaskRef<C> {}
-
-impl<C: Send> fmt::Show for CapTaskRef<C> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { Ok(()) }
-}
-
-pub struct CapSet<T, R> {
-    cap_types: (),
-    cap_ref: R,
-}
-
-impl<T, C: fmt::Show> fmt::Show for CapSet<T, Box<CapRef<C> + Send>> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { Ok(()) }
-}
-
-#[unsafe_destructor]
-impl<T, C: Command<T> + Send> Drop for CapTaskRef<C> {
-    fn drop(&mut self) {} 
-}
-
-pub trait Actor<T, C: Command<T>>: fmt::Show + Send {
-    fn spawn_actor(actor: Self) -> CapSet<(), Box<CapRef<C> + Send>> {
-        let (tx, rx) = channel();
-        let tx_clone = tx.clone();
-        spawn(proc() {
-            let mut actor = actor;
-            let mut iter : Messages<CmdWrap<C>> = rx.iter();
-        });
-        let cap_set : CapSet<(), Box<CapRef<C> + Send>> = CapSet { cap_types: (), cap_ref: box CapTaskRef { tx: tx_clone } as Box<CapRef<C> + Send> };
-        cap_set
-    }
-}
-
-impl Command<()> for () {}
-impl Actor<(), ()> for () {}
 
 fn main() {
-    let itemcap = Actor::spawn_actor(());
+    trait Trait<T> {}
+    struct Struct<S>(S);
+
+    #[unsafe_destructor]
+    impl<S, T: Trait<S>> Drop for Struct<T> {
+        fn drop(&mut self) {}
+    }
+    impl<S> Trait<S> for Struct<S> {}
+
+    box Struct(()) as Box<Trait<()>>;
 }
