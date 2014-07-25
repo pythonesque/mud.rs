258eb3f15e78e56e4071d5ccf31598f13baa569f
Joshua Yanovski <pythonesque@gmail.com>
2014-07-25 00:54:25 -0700

    DRYing main() a bit.

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ mod core;
 green_start!(main)
 
 fn main() {
-    let item_data = Item::make(ItemData);
+    static NUM_ITEMS: uint = 10_000_000;
+    static NUM_TASKS: uint = 8;
+    static ITEM_DATA: ItemData = ItemData;
+    let item_data = Item::make(ITEM_DATA);
     let mut item = Actor::make_actor(item_data);
-    range(0u, 16).map( |i| {
+    range(0u, NUM_TASKS).map( |i| {
+        static ITEMS_PER_TASK: uint = NUM_ITEMS / NUM_TASKS;
         Future::spawn(proc() {
+            let mut item = Actor::make_actor(Item::make(ITEM_DATA));
             println!("Creating item {}", i);
-            let mut item = Actor::make_actor(Item::make(ItemData));
-            for _ in range(0u, 612_500) {
-                let new_item = if /*i % 2 == 0*/false {
-                    Actor::spawn_actor(Item::make(ItemData))
-                } else {
-                    Actor::make_actor(Item::make(ItemData))
-                };
+            for _ in range(0u, ITEMS_PER_TASK) {
+                let new_item = Actor::make_actor(Item::make(ItemData));
                 item.send_cmd(item::Give(new_item)).unwrap();
             }
             println!("Done populating item {}", i);
@@ -40,4 +40,5 @@ fn main() {
     let mob = Mob::make(MobData { title: "Zombie".into_maybe_owned(), desc: "Shuffling aimlessly.".into_maybe_owned()});
     let mut mobcap = Actor::make_actor(mob);
     mobcap.send_cmd(mob::Give(item)).unwrap();
+    println!("Done.");
 }
