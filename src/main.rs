0746c130751e32a71c500cdac290bf618f428203
Joshua Yanovski <pythonesque@gmail.com>
2014-07-24 19:27:12 -0700

    Demonstration of effective data parallel item insertions.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use core::item;
 use core::item::{Item, ItemData};
 use core::mob;
 use core::mob::{Mob, MobData};
+use std::sync::Future;
 
 mod core;
 
@@ -15,17 +16,28 @@ green_start!(main)
 
 fn main() {
     let item_data = Item::make(ItemData);
-    let mut item = Actor::spawn_actor(item_data);
-    for i in range(0u, 2) {
-        let new_item = if i % 2 == 0 {
-            Actor::spawn_actor(Item::make(ItemData))
-        } else {
-            Actor::make_actor(Item::make(ItemData))
-        };
-        item.send_cmd(item::Give(new_item)).unwrap();
-    }
+    let mut item = Actor::make_actor(item_data);
+    range(0u, 16).map( |i| {
+        Future::spawn(proc() {
+            println!("Creating item {}", i);
+            let mut item = Actor::make_actor(Item::make(ItemData));
+            for _ in range(0u, 612_500) {
+                let new_item = if /*i % 2 == 0*/false {
+                    Actor::spawn_actor(Item::make(ItemData))
+                } else {
+                    Actor::make_actor(Item::make(ItemData))
+                };
+                item.send_cmd(item::Give(new_item)).unwrap();
+            }
+            println!("Done populating item {}", i);
+            item
+        })
+    }).collect::<Vec<Future<_>>>()
+    .move_iter().map( |future| future.unwrap() )
+    .map( |new_item| item::Give(new_item) )
+    .map( |new_item| item.send_cmd(new_item) )
+    .all( |result| result.is_ok() );
     let mob = Mob::make(MobData { title: "Zombie".into_maybe_owned(), desc: "Shuffling aimlessly.".into_maybe_owned()});
     let mut mobcap = Actor::make_actor(mob);
     mobcap.send_cmd(mob::Give(item)).unwrap();
-    println!("{}", mobcap);
 }
